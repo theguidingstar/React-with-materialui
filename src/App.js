d6386b15cd74001be845f5897b248553d060c079
Yash Shah <yashshh123@gmail.com>
2022-05-15 19:27:27 +0530

    Updated UI for the Application Homepage

--- src/App.js
+++ src/App.js
@@ -6,19 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>React Boilerplate with Material-ui Integration</p>
         <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
+          Visit &nbsp;
+          <a
+            href="https://github.com/theguidingstar/React-with-materialui"
+            target="_black"
+          >
+            Github
+          </a>
+          &nbsp; for detailed documentation
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   )
